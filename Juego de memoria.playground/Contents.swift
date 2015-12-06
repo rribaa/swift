8feb702a4fd87030d2aeffad6393e75384831079
Ramon Riba <ramonriba@MacBook-Pro-de-Ramon.local>
2015-12-06 18:56:49 +0100

    Inicio

---
+++ Juego de memoria.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+for i in 0...100 {
+    if i % 5 == 0 {
+        print("\(i) Bingo!!!")
+    } else if i % 2 == 0 {
+        print("\(i) par!!!")
+    } else if i % 2 == 1 {
+        print("\(i) impar!!!")
+    } else if i >= 30 && i <= 40 {
+        print("\(i) Viva swift!!!")
+    }
+    
+    
+}
